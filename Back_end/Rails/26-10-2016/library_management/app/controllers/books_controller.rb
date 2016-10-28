bafc80e400ac3fe468752c4d1b9a9796dc21ccff
Chethan <crajendra@qwinix.io>
2016-10-28 09:22:21 +0530

    first

---
+++ Back_end/Rails/26-10-2016/library_management/app/controllers/books_controller.rb
@@ -0,0 +1,47 @@
+class BooksController < ApplicationController
+  
+  def index
+  	@books = Book.all
+  end
+
+  def new
+  	@book = Book.new
+  end
+
+  def create
+  	# binding.pry
+  	@book = Book.new(book_params)
+  	@book.save
+  	redirect_to books_list_path
+  end
+  
+  def show
+  @book = Book.find(params[:id])
+  end  
+
+  def edit
+  	@book = Book.find_by_id(params[:id])
+  end
+
+  def update
+
+    binding.pry
+    @book = Book.find_by_id(params[:id])
+    @book.update_attributes(book_params)
+    redirect_to books_list_path
+
+  end
+
+  def destroy
+    @book = Book.find_by_id(params[:id])
+    @book.destroy
+    redirect_to books_list_path
+  end
+
+  private
+
+  def book_params
+  	params.require(:book).permit(:book_name,:author,:publication)
+  end
+
+end
--- Front_end/Assignment/Assignment_2_21-10-2016/script.js
+++ Front_end/Assignment/Assignment_2_21-10-2016/script.js
@@ -1,36 +1,4 @@
-var myIndex = 0;
-carousel();
-
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";
-    }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1}
-    x[myIndex-1].style.display = "block";
-    setTimeout(carousel, 2000); // Change image every 2 seconds
-}
-
-
-$('#menucontact').click(function(){
-	$('#homePage').hide();
-	$('#contact').show();
-	$('#aboutPage').hide();
-
+$('.scrollToTop').click(function(){
+    $('html, body').animate({scrollTop : 0},800);
+    return false;
 });
-
-$('#menuabout').click(function(){
-	$('#aboutPage').show();
-	$('#homePage').hide();
-	$('#contact').hide();
-
-});
-
-$('#menuhome').click(function(){
-	$('#homePage').show();
-	$('#aboutPage').hide();
-	$('#contact').hide();
-})
-
